69fdf7364417a5b54fec1c267987f1f292630ceb
Chance093 <chance.dev093@gmail.com>
2025-06-11 11:08:31 -0700

    feat: add initial content for terminal

---
+++ public/content.js
@@ -0,0 +1,68 @@
+export const portfolio = {
+    projects: {
+        type: "dir",
+        content: "",
+        projects: [
+            {
+                name: "Proglo Shipping",
+                description: "",
+                link: "",
+            },
+        ],
+    },
+    about: {
+        type: "dir",
+        aboutMe: {
+            name: "Chance Espinosa",
+            content: "",
+            languages: [],
+            frameworks: [],
+            tools: [],
+        },
+        aboutPortfolio: "",
+    },
+    resume: {
+        type: "file",
+        name: "Chance Espinosa",
+        location: "Las Vegas, Nevada",
+        email: "chance.dev093@gmail.com",
+        description: "",
+        education: {
+            school: "Pahrump Valley High School",
+            date: "August 2013 - May 2017",
+            location: "Pahrump, Nevada",
+            description: "Graduated with a 3.6 GPA and received the Honors Diploma from Pahrump Valley High School.",
+            obtained: "High School Diploma",
+        },
+        experience: [
+            {
+                company: "",
+                position: "",
+                date: "",
+                location: "",
+                bulletPoints: [""],
+            },
+        ],
+        skills: [""],
+    },
+    contact: {
+        type: "file",
+        content: "",
+        name: "Chance Espinosa",
+        location: "Las Vegas, Nevada",
+        email: "chance.dev093@gmail.com",
+        linkedin: "",
+        github: "http://github.com/Chance093",
+    },
+    blog: {
+        type: "dir",
+        content: "",
+        link: "https://github.com/Chance093/markdown",
+        blogs: [
+            {
+                title: "Managing Multiple SSH Keys for GitHub and GitLab",
+                link: "https://github.com/Chance093/markdown/blob/main/HOW_TO_MANAGE_SSH_KEYS.md",
+            },
+        ],
+    },
+};
--- public/index.js
+++ public/index.js
@@ -1,12 +1,33 @@
 "use strict";
-async function renderPage() {
+async function renderTerminal() {
     const terminal = document.getElementById("terminal");
-    if (!terminal) {
+    if (!terminal)
         throw new Error("Terminal not set in HTML");
-    }
-    terminal.textContent += "\n\n";
-    await renderIntroText(terminal, 70);
+    await renderIntroText(terminal, 1);
     renderInputLine(terminal);
+}
+function renderInputLine(term) {
+    // create input line elements
+    const div = createElement("div", ["inputLine"]);
+    const user = createElement("p", ["user"], "user@root  $");
+    const input = createElement("input", ["input"]);
+    input.setAttribute("type", "text");
+    // append to each other
+    div.appendChild(user);
+    div.appendChild(input);
+    term.appendChild(div);
+    // focus cursor on input
+    input.focus();
+}
+function createElement(el, classes, text) {
+    const element = document.createElement(el);
+    if (classes.length > 0) {
+        element.classList.add(...classes);
+    }
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
 }
 function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
@@ -18,18 +39,4 @@ async function renderIntroText(el, ms) {
         el.textContent += TEXT[i];
     }
 }
-function renderInputLine(el) {
-    const div = document.createElement("div");
-    div.classList.add("inputLine");
-    const user = document.createElement("p");
-    user.textContent = "user@root  $";
-    user.classList.add("user");
-    div.appendChild(user);
-    const input = document.createElement("input");
-    input.setAttribute("type", "text");
-    input.classList.add("input");
-    div.appendChild(input);
-    el.appendChild(div);
-    input.focus();
-}
-renderPage();
+renderTerminal();
